e8b1285b3cdab4f8a5c8eb6354f1d08eeb655eba
Michelle Leano <leanomc@hawaii.edu>
2022-02-26 12:00:21 -1000

    icspark iffy

--- app.js
+++ app.js
@@ -1,30 +1,56 @@
-(function() {
-    "use strict";
+// (function() {
+//     "use strict";
+//
+//     window.onload = function() {
+//         document.getElementById('myBtn').onclick = getShape;
+//     };
+//
+//     function getShape() {
+//         let input = document.getElementById('userInput').value;
+//         let solution = document.getElementById('solution');
+//
+//         if (input == 3) {
+//             solution.innerHTML = "Triangle";
+//         } else if (input == 4) {
+//             solution.innerHTML = "Quadrilateral";
+//         } else if (input == 5) {
+//             solution.innerHTML = "Pentagon";
+//         } else if (input == 6) {
+//             solution.innerHTML = "Hexagon";
+//         } else if (input == 7) {
+//             solution.innerHTML = "Heptagon";
+//         } else if (input == 8) {
+//             solution.innerHTML = "Octagon";
+//         } else if (input == 9) {
+//             solution.innerHTML = "Nonagon";
+//         } else if (input == 10) {
+//             solution.innerHTML = "Decagon";
+//         } else {
+//             solution.innerHTML = "Try Again!";
+//         }
+//     }
+// })();
+document.getElementById("myBtn").addEventListener("click", function() {
+  let input = document.getElementById("userInput").value;
+  let solution = document.getElementById("solution");
 
-    var input = document.getElementById('userInput');
-    var button = document.getElementById('solve');
-    var solution = document.getElementById('solution');
-
-    document.getElementById('solve').addEventListener("click", function() {
-        if (input == 3) {
-            solution.innerHTML = "Triangle";
-        } else if (input == 4) {
-            solution.innerHTML = "Quadrilateral";
-        } else if (input == 5) {
-            solution.innerHTML = "Pentagon";
-        } else if (input == 6) {
-            solution.innerHTML = "Hexagon";
-        } else if (input == 7) {
-            solution.innerHTML = "Heptagon";
-        } else if (input == 8) {
-            solution.innerHTML = "Octagon";
-        } else if (input == 9) {
-            solution.innerHTML = "Nonagon";
-        } else if (input == 10) {
-            solution.innerHTML = "Decagon";
-        } else {
-            solution.innerHTML = "Try Again!";
-        }
-    });
-
-})();
+  if (input == 3) {
+    solution.innerHTML = 'Triangle';
+  } else if (input == 4) {
+    solution.innerHTML = 'Quadrilateral';
+  } else if (input == 5) {
+    solution.innerHTML = 'Pentagon';
+  } else if (input == 6) {
+    soltuion.innerHTML = 'Hexagon';
+  } else if (input == 7) {
+    solution.innerHTML = 'Heptagon';
+  } else if (input == 8) {
+    solution.innerHTML = 'Octagon';
+  } else if (input == 9) {
+    solution.innerHTML = 'Nonagon';
+  } else if (input == 10) {
+    solution.innerHTML = 'Decagon';
+  } else {
+    solution.innerHTML = 'Try Again';
+  }
+});
